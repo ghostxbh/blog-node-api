6fe35ebe53bacbc17f5af45cb3fbee0dc674f3d4
ghostxbh <ghostxbh@hotmail.com>
2019-06-17 22:15:42 +0800

    增加home接口，删除其他

---
+++ routes/font/home.js
@@ -0,0 +1,8 @@
+/**
+ * Created by xbh 2019-06-17
+ */
+var express = require('express');
+var router = express.Router();
+const result = require('../../util/result-util');
+
+module.exports = router;
---
+++ service/home-service.js
@@ -0,0 +1,16 @@
+/**
+ * Created by xbh 2019-06-17 首页业务
+ */
+const contentDao = require('../dao/contents-dao');
+const categoryDao = require('../dao/category-dao');
+const linksDao = require('../dao/links-dao');
+const labelsDao = require('../dao/labels-dao');
+const specialDao = require('../dao/special-dao');
+
+const homeService = {
+    home: async () => {
+
+    },
+};
+
+module.exports = homeService;
