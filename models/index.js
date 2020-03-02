f6d3cce53c336b5ed26942dcf8f65ed33325b693
ghostxbh <ghostxbh@hotmail.com>
2020-03-02 23:29:58 +0800

    add model index

---
+++ models/index.js
@@ -0,0 +1,18 @@
+/**
+ * @author ghostxbh
+ * @date 2020/2/11
+ * @description model index
+ */
+const category = require('./category');
+const docment = require('./docment');
+const special = require('./special');
+const label = require('./label');
+const link = require('./link');
+
+module.exports = {
+    category,
+    docment,
+    special,
+    label,
+    link
+};
